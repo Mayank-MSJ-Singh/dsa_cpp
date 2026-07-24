92af4d519529f2f6b16bc31aa7bc9a6005b9212c
Mayank Singh Jadon <mayank.msj.singh@gmail.com>
2026-07-24 16:23:50 +0530

    feat: update subset algorithm to handle duplicate elements using iteration skipping

--- recursion/subset.cpp
+++ recursion/subset.cpp
@@ -14,11 +14,15 @@ void subset(vector<int> &arr, vector<int> &ans, int i) {
   ans.push_back(arr[i]);
   subset(arr, ans, i + 1);
   ans.pop_back();
-  subset(arr, ans, i + 1);
+  i += 1;
+  while (i < arr.size() && arr[i] == arr[i - 1]) {
+    i += 1;
+  }
+  subset(arr, ans, i);
 }
 
 int main() {
-  vector<int> arr = {1, 2, 3, 4};
+  vector<int> arr = {1, 2, 2};
   vector<int> ans;
   subset(arr, ans, 0);
   return 0;
